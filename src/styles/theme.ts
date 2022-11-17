eba0cd59344dfbde535ad81e90e2d4c9c734843a
Ulisses <ulissessuporteti01@gmail.com>
2022-11-17 20:13:07 -0300

    declarando algumas variáveis de estilo em theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,25 @@
+export const theme = {
+  colors: {
+    aFont: "#E6E6E6",
+    CinzaEscuro: "#363636",
+    black: "#000000",
+    btnEntrarActive: "#37E2D5",
+    cinzaClaro: "#C5C5C5",
+    purple: "#7D2CFF",
+    red: "#F24E1E",
+    verdeNeon: "#49FF00",
+    white: "#FFFFFF",
+  },
+  padding: {
+    pd10: "10px",
+    pd15: "15px",
+    pd30: "15px",
+  },
+  borders: {
+    rd8: "8px",
+    rd10: "10px",
+  },
+  shadows: {
+    defaultBoxShadow: "0px 4px 4px 0px #00000040",
+  },
+};
