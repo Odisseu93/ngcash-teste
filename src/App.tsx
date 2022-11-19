bd80f09246b3044c33d1806c9057149206e46996
Ulisses <ulissessuporteti01@gmail.com>
2022-11-19 08:05:06 -0300

    criando as rotas em App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
-function App() {
+import { ThemeProvider } from "styled-components";
+import { AuthProvider } from "./assets/context/AuthProvider";
+import { Cadastro } from "./pages/Cadastro/ixdex";
+import { Login } from "./pages/Login";
+import GlobalStyle from "./styles/global";
+import { theme } from "./styles/theme";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Logado } from "./components/Private/Logado";
 
+function App() {
   return (
-    <>Hello world!</>
-  )
+    <>
+      <GlobalStyle />
+      <AuthProvider>
+        <Router>
+          <ThemeProvider theme={theme}>
+            <Routes>
+              <Route path={"/cadastro"} element={<Cadastro />} />
+              <Route path={"/login"} element={<Login />} />
+              <Route
+                path={"/logado"}
+                element={
+                  <Logado>
+                    <h2>teste componente logado!</h2>
+                  </Logado>
+                }
+              />
+            </Routes>
+          </ThemeProvider>
+        </Router>
+      </AuthProvider>
+    </>
+  );
 }
 
-export default App
+export default App;
